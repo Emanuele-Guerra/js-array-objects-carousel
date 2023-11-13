bb18eaab61bf33f64b7b9385daf634c56f539089
Emanuele-Guerra <emaguerra1993@hotmail.it>
2023-11-13 18:24:58 +0100

    push2

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
 let box = document.getElementById("container")
-let imgBox = document.createElement("img")
+let imgBox = document.createElement('img')
+imgBox.classList.add("images")
 let next = document.getElementById("nextArrow")
 let back = document.getElementById("backArrow")
 let play = document.getElementById("play")
-
+let paragrafo = document.getElementById("paragrafo")
+let titolo = document.getElementById("titolo")
 
 
 const images = [
     {
         image: 'img/01.webp',
-        title: 'Marvel\'s Spiderman Miles Morale',
+        title: 'Marvel\'s Spiderman Miles Morales',
         text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
     }, {
         image: 'img/02.webp',
@@ -28,11 +30,22 @@ const images = [
         title: "Marvel's Avengers",
         text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
     }
-];
+];          
+    images.forEach((element)=>{
+       
+        
+        
+        
+        imgBox.src= element.image
+        box.appendChild(imgBox)
+        titolo.innerHTML= element.title
+        paragrafo.innerHTML= element.text
 
-images.forEach((element)=>{
+        
+    })    
+        //let immagine = element.image
+        //let titolo = element.title
+        //let testo = element.text
+        
+        //image++
     
-    
-
-
-})
